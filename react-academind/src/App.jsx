ce05f9f86dca030e3bbe76292f290764b926051d
Hugo Tiburcio OSF <104787924+hugotiburcio@users.noreply.github.com>
2022-10-17 22:01:17 -0300

    adding components

---
+++ react-academind/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-academind/src/components/ExpenseItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ExpenseItem.module.css";
+
+function ExpenseItem() {
+  return <div>ExpenseItem</div>;
+}
+
+export default ExpenseItem;
